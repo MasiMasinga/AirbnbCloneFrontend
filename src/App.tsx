325a6d7cd8c78e5d32014e8edb1d9bb8a1a2095e
Masi <51009658+MasiMasinga@users.noreply.github.com>
2026-08-23 14:46:39 +0200

    feat: add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 // Routes
 import PageRoutes from "./routes/routes";
 
+// Tanstack Query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 // Snackbar
 import { SnackbarProvider } from "notistack";
 
@@ -9,11 +12,13 @@ import { AuthProvider } from "./common/contexts/AuthContext";
 
 function App() {
     return (
-        <SnackbarProvider maxSnack={3}>
-            <AuthProvider>
-                <PageRoutes />
-            </AuthProvider>
-        </SnackbarProvider>
+        <QueryClientProvider client={new QueryClient()}>
+            <SnackbarProvider maxSnack={3}>
+                <AuthProvider>
+                    <PageRoutes />
+                </AuthProvider>
+            </SnackbarProvider>
+        </QueryClientProvider>
     );
 }
 
