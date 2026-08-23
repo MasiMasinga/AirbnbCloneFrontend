3fb9294bdc94a7112377e6e49aeb19c1684492b8
Masi <51009658+MasiMasinga@users.noreply.github.com>
2026-08-23 14:42:01 +0200

    feat: add new package

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { Button } from "@astryxdesign/core/Button";
-import { VStack } from "@astryxdesign/core/Layout";
-import { Stack } from "@astryxdesign/core/Layout";
-
 // Routes
 import PageRoutes from "./routes/routes";
 
+// Snackbar
+import { SnackbarProvider } from "notistack";
+
+// Context
+import { AuthProvider } from "./common/contexts/AuthContext";
+
 function App() {
     return (
-        <Stack direction="vertical" gap={4} height="100vh">
-            <VStack gap={2}>
-                <Button label="Hello Astryx" onClick={() => alert("Hi!")} />
-            </VStack>
-            <PageRoutes />
-        </Stack>
+        <SnackbarProvider maxSnack={3}>
+            <AuthProvider>
+                <PageRoutes />
+            </AuthProvider>
+        </SnackbarProvider>
     );
 }
 
