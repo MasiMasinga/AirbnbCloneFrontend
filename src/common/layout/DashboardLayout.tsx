b8cde0c05238ca734b792e233c9ba1be5c6c13f3
Masi <51009658+MasiMasinga@users.noreply.github.com>
2026-08-26 01:20:32 +0200

    update navbar items

--- src/common/layout/DashboardLayout.tsx
+++ src/common/layout/DashboardLayout.tsx
@@ -1,8 +1,17 @@
-// React Router Dom
+// React Router
 import { Outlet, useLocation } from "react-router";
 
 // Astryx
-import { AppShell, SideNav, TopNav, TopNavHeading } from "@astryxdesign/core";
+import {
+    AppShell,
+    SideNav,
+    TopNav,
+    TopNavHeading,
+    TopNavMenu,
+} from "@astryxdesign/core";
+
+// Lucide Icons
+import { UserCircleIcon, LogOut } from "lucide-react";
 
 // Components
 import GuestDrawerLinkList from "./containers/GuestDrawerLinkList";
@@ -18,12 +27,37 @@ const DashboardLayout = () => {
             contentPadding={4}
             topNav={
                 <TopNav
-                    heading={<TopNavHeading heading="AirbnbClone" headingHref="/" />}
+                    heading={
+                        <TopNavHeading heading="AirbnbClone" headingHref="/" />
+                    }
+                    endContent={
+                        <TopNavMenu
+                            label="Profile"
+                            items={[
+                                {
+                                    title: "Account",
+                                    icon: <UserCircleIcon />,
+                                    href: isHostMode
+                                        ? "/host/account"
+                                        : "/guest/account",
+                                },
+                                {
+                                    title: "Logout",
+                                    icon: <LogOut />,
+                                    href: "/login",
+                                },
+                            ]}
+                        />
+                    }
                 />
             }
             sideNav={
                 <SideNav>
-                    {isHostMode ? <HostDrawerLinkList /> : <GuestDrawerLinkList />}
+                    {isHostMode ? (
+                        <HostDrawerLinkList />
+                    ) : (
+                        <GuestDrawerLinkList />
+                    )}
                 </SideNav>
             }
         >
