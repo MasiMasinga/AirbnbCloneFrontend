c8958a5947ec7a344ebeb5482af38dcb35ee5457
Masi <51009658+MasiMasinga@users.noreply.github.com>
2026-09-25 04:04:28 +0200

    remove unused stores

--- src/stores/useAuthStore.ts
+++
@@ -1,5 +0,0 @@
-import { create } from 'zustand';
-
-export const useAuthStore = create((set) => ({
-    setAuthToken: (token: string) => set({ authToken: token }),
-}));
--- src/stores/useSupportStore.ts
+++
@@ -1,5 +0,0 @@
-import { create } from 'zustand';
-
-export const useSupportStore = create((set) => ({
-    setSupportTicket: (ticket: any) => set({ supportTicket: ticket }),
-})); 
